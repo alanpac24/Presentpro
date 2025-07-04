e8b4094de51fc02e352f41d24f651bde16d20fe4
Alan Yang <alan.yang91@gmail.com>
2025-07-05 03:49:03 +1000

    fix: Resolve type export conflict

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,21 @@
 
 export * from './user'
 export * from './database'
-export * from './presentation'
+// Re-export presentation types except those that conflict with database
+export {
+  type Slide,
+  type SlideType,
+  type SlideElement,
+  type ElementType,
+  type ChartType,
+  type ShapeType,
+  type Position,
+  type Size,
+  type ElementStyle,
+  type ChartData,
+  type TableData,
+  type Presentation,
+  type Template,
+  type TemplateCategory,
+  type PresentationAction
+} from './presentation'
